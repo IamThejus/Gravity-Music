bb79b7fb4577aac764ecb7d8aa4e628351fb84f7
Anuragh K P <kpanuragh@gmail.com>
2026-06-18 09:45:31 +0530

    feat(linux): window title 'Gravity Music' + 800x600 min size

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,14 +40,22 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "SaraGama");
+    gtk_header_bar_set_title(header_bar, "Gravity Music");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "SaraGama");
+    gtk_window_set_title(window, "Gravity Music");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+
+  // Keep the adaptive layout from collapsing: never allow the window smaller
+  // than 800x600.
+  GdkGeometry geometry;
+  geometry.min_width = 800;
+  geometry.min_height = 600;
+  gtk_window_set_geometry_hints(window, nullptr, &geometry, GDK_HINT_MIN_SIZE);
+
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
